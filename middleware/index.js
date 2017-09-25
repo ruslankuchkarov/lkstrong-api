0701702a91e21fcf3613e847d5c6af9ca09259b2
ruslankuchkarov <kuchkarov.r@gmail.com>
2017-09-25 20:40:11 +0700

    Update index.js

--- middleware/index.js
+++ middleware/index.js
@@ -28,8 +28,8 @@ module.exports = function (app, express) {
     app.use(bodyParser.json());
     app.use(require('../controllers'))
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-    app.use(express.static(path.join(__dirname, '../public')))
-    app.use("/public", express.static(path.join(__dirname, '../public')))
+//     app.use(express.static(path.join(__dirname, '../public')))
+//     app.use("/public", express.static(path.join(__dirname, '../public')))
     mongoose.connect(config.get('db:url'), { useMongoClient: true })
     db.on('error', console.error.bind(console, 'MongoDB connection error: '))
 };
