1c805836123e7d07c9382261b09563c213cb858a
Кучкаров Руслан Рамазанович <r.kuchkarov@2gis.ru>
2017-10-24 19:04:04 +0700

    cors

--- middleware/index.js
+++ middleware/index.js
@@ -23,7 +23,14 @@ module.exports = function (app, express) {
         res.setHeader('Content-Type', 'application/json')
         res.send(swaggerSpec)
     })
+    var allowCrossDomain = function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    }
 
+    app.use(allowCrossDomain);
     app.use(bodyParser.json());
     app.use(require('../controllers'))
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
